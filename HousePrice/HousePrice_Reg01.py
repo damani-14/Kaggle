330d7202734e1dd4d9438fe57996ac55591a261e
damani-14 <damanidriver@gmail.com>
2018-08-15 18:07:47 -0500

    Updated model 01

--- HousePrice/HousePrice_Reg01.py
+++ HousePrice/HousePrice_Reg01.py
@@ -8,7 +8,39 @@ from sklearn import linear_model
 
 def main():
 
+# Data Exploration
+#-----------------
+
+    # Importing Data
     train = pd.read_csv('train.csv')
     test = pd.read_csv('test.csv')
+
+    # Set plot parameters
+    plt.style.use(style='ggplot')
+    plt.rcParams['figure.figsize'] = (5, 3)
+
+    # Investigating response distribution
+    plt.hist(train.SalePrice, color='blue')
+    plt.show()
+
+        # NOTE: Response variable is skewed
+    print(train.SalePrice.skew(),'\n')
+
+        # Adjust for Skew
+    response = np.log(train.SalePrice)
+
+        # Check
+    print(train.SalePrice.skew(),'\n')
+
+# Feature Selection
+#------------------
+
+    # Quantitative Variables
+    numeric_features = train.select_dtypes(include = (np.number))
+    corr = numeric_features.corr()
+
+        # Investigating the most positive and most negative correlated variables
+    print(corr['SalePrice'].sort_values(ascending=False)[:5], '\n')
+    print(corr['SalePrice'].sort_values(ascending=False)[-5:], '\n')
 
 main()
